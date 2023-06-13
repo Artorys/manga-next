d858912f1d112f3f2aec10f76a5ba9c122735099
Artorys <darksoulsgui123@gmail.com>
2023-06-13 17:37:56 -0300

    feat: add all logic for the aside when we click in the menu item

--- src/components/Aside/index.tsx
+++ src/components/Aside/index.tsx
@@ -1,38 +1,54 @@
 "use client"
+import { AsideContext } from "@/contexts/aside.context";
 import { Flex,Text } from "@chakra-ui/react";
+import { usePathname } from "next/navigation";
+import { useContext, useEffect } from "react";
 import { BsFillPeopleFill } from "react-icons/bs";
 import { CgProfile, CgUserList } from "react-icons/cg";
 import { FaCompass, FaDonate } from "react-icons/fa";
 import { IoMdNotifications, IoMdSettings } from "react-icons/io";
+import style from "./style.module.css"
 
 
 export function Aside(){
+
+    const {setItemsStyle,styleItems} = useContext(AsideContext)
+    const pathName = usePathname()
+
+    useEffect(()=>{
+      for(const key in styleItems){
+        if(key === pathName){
+          setItemsStyle((old)=> {
+           return {...old,[key] : style.sidebar_item}
+          })
+        }
+      }
+    },[pathName])
+
     return (
-        <Flex>
         <Flex paddingLeft={{tabletLarge : "1.9375rem",default : "0.5rem"}} paddingRight={"0.5rem"}  paddingTop={`6rem`}>
-          <Flex>
-            <Flex flexDirection={`column`} gap={"2.5rem"}>
-              <Flex flexDirection={`column`} gap={"1rem"}>
+          <Flex width={"100%"} flexDirection={`column`} gap={"2.5rem"}>
+              <Flex width={"100%"} flexDirection={`column`} gap={"1rem"}>
                 <Text letterSpacing={`1px`} color={"greyScale.grey10"} fontWeight={`menuBar.text`} fontSize={`menuBar.text`}>
                     Menu -
                 </Text>
-                <Flex alignItems={`center`} gap={`1rem`}>
+                <Flex className={`${styleItems["/home"]} ${style.item}`} alignItems={`center`} gap={`1rem`}>
                   <Text margin={0} letterSpacing={`1px`} color={"greyScale.whiteFixed"} fontWeight={`menuBar.icon`} fontSize={`menuBar.icon`}>
                     雪
                   </Text>
                   <Text margin={0} letterSpacing={`1px`} color={"greyScale.whiteFixed"} fontWeight={`menuBar.text`} fontSize={`menuBar.text`}>
                     Home
-                </Text>
+                  </Text>
                 </Flex>
-                <Flex alignItems={`center`} gap={`1rem`}>
+                <Flex className={`${styleItems["/discover"]} ${style.item}`}  alignItems={`center`} gap={`1rem`}>
                   <Text margin={0} letterSpacing={`1px`} color={"greyScale.whiteFixed"} fontWeight={`menuBar.icon`} fontSize={`menuBar.icon`}>
                     <FaCompass></FaCompass>
                   </Text>
                   <Text margin={0} letterSpacing={`1px`} color={"greyScale.whiteFixed"} fontWeight={`menuBar.text`} fontSize={`menuBar.text`}>
                     Discover comics
-                </Text>
+                  </Text>
                 </Flex>
-                <Flex alignItems={`center`} gap={`1rem`}>
+                <Flex className={`${styleItems["/authors"]} ${style.item}`}  alignItems={`center`} gap={`1rem`}>
                     <Text margin={0} letterSpacing={`1px`} color={"greyScale.whiteFixed"} fontWeight={`menuBar.icon`} fontSize={`menuBar.icon`}>
                       <BsFillPeopleFill></BsFillPeopleFill>
                     </Text>
@@ -40,7 +56,7 @@ export function Aside(){
                       Authors
                     </Text>
                 </Flex>
-                <Flex alignItems={`center`} gap={`1rem`}>
+                <Flex className={`${styleItems["/notifications"]} ${style.item}`}  alignItems={`center`} gap={`1rem`}>
                   <Text margin={0} letterSpacing={`1px`} color={"greyScale.whiteFixed"} fontWeight={`menuBar.icon`} fontSize={`menuBar.icon`}>
                     <IoMdNotifications></IoMdNotifications>
                   </Text>
@@ -49,11 +65,11 @@ export function Aside(){
                 </Text>
                 </Flex>
               </Flex>
-              <Flex flexDirection={`column`} gap={"1rem"}>
+              <Flex  flexDirection={`column`} gap={"1rem"}>
                 <Text letterSpacing={`1px`} color={"greyScale.grey10"} fontWeight={`menuBar.text`} fontSize={`menuBar.text`}>
                     General -
                 </Text>
-                <Flex alignItems={`center`} gap={`1rem`}>
+                <Flex className={`${styleItems["/profile"]} ${style.item}`} alignItems={`center`} gap={`1rem`}>
                   <Text margin={0} letterSpacing={`1px`} color={"greyScale.whiteFixed"} fontWeight={`menuBar.icon`} fontSize={`menuBar.icon`}>
                     <CgProfile></CgProfile>
                   </Text>
@@ -61,7 +77,7 @@ export function Aside(){
                     Profile
                 </Text>
                 </Flex>
-                <Flex alignItems={`center`} gap={`1rem`}>
+                <Flex className={`${styleItems["/mylist"]} ${style.item}`} alignItems={`center`} gap={`1rem`}>
                   <Text margin={0} letterSpacing={`1px`} color={"greyScale.whiteFixed"} fontWeight={`menuBar.icon`} fontSize={`menuBar.icon`}>
                     <CgUserList></CgUserList>
                   </Text>
@@ -69,7 +85,7 @@ export function Aside(){
                     My List
                 </Text>
                 </Flex>
-                <Flex alignItems={`center`} gap={`1rem`}>
+                <Flex className={`${styleItems["/settings"]} ${style.item}`} alignItems={`center`} gap={`1rem`}>
                     <Text margin={0} letterSpacing={`1px`} color={"greyScale.whiteFixed"} fontWeight={`menuBar.icon`} fontSize={`menuBar.icon`}>
                       <IoMdSettings></IoMdSettings>
                     </Text>
@@ -77,7 +93,7 @@ export function Aside(){
                       Settings
                     </Text>
                 </Flex>
-                <Flex alignItems={`center`} gap={`1rem`}>
+                <Flex className={`${styleItems["/donate"]} ${style.item}`} alignItems={`center`} gap={`1rem`}>
                   <Text margin={0} letterSpacing={`1px`} color={"greyScale.whiteFixed"} fontWeight={`menuBar.icon`} fontSize={`menuBar.icon`}>
                     <FaDonate></FaDonate>
                   </Text>
@@ -88,7 +104,5 @@ export function Aside(){
               </Flex>
             </Flex>
           </Flex>
-        </Flex>
-      </Flex>
     )
 }
--- src/components/SideBar/index.tsx
+++ src/components/SideBar/index.tsx
@@ -25,7 +25,7 @@ export function SideBar(){
           })
         }
       }
-    },[])
+    },[pathName])
 
 
   return (
